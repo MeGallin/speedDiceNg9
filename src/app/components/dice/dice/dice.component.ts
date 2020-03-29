2c331a8a378ae733b4799dbd2f599d4009481d17
Guy <me@garyallin.uk>
2020-03-29 09:41:28 +0100

    initial commit

---
+++ src/app/components/dice/dice/dice.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice',
+  templateUrl: './dice.component.html',
+  styleUrls: ['./dice.component.css']
+})
+export class DiceComponent implements OnInit {
+  public diceOne: number;
+  public diceOneView: number;
+  public diceTwoView: number;
+  public diceThreeView: number;
+  public diceTwo: number;
+  public diceThree: number;
+  public total: number;
+  public double: boolean;
+  public triple: boolean;
+  public tripleMessage: string = "You're a triple WINNA! Go straight to HOME!";
+  public doubleMessage: string = 'Double spin again';
+  public maxSpinMessage: string =
+    'Moola! Get 500 from each player and GO to HOME!';
+  public passOnDice: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  spinDice() {
+    this.diceOne = Math.floor(Math.random() * 6 + 1);
+    this.diceTwo = Math.floor(Math.random() * 6 + 1);
+    this.diceThree = Math.floor(Math.random() * 6 + 1);
+
+    this.total = null;
+    this.diceOneView = this.diceOne;
+    this.diceTwoView = this.diceTwo;
+    this.diceThreeView = this.diceThree;
+
+    setTimeout(() => {
+      this.diceOneView = null;
+    }, 500);
+
+    setTimeout(() => {
+      this.diceTwoView = null;
+    }, 1000);
+
+    setTimeout(() => {
+      this.diceThreeView = null;
+    }, 1500);
+
+    setTimeout(() => {
+      this.total = this.diceOne + this.diceTwo + this.diceThree;
+    }, 2000);
+
+    // Checking for doubles
+    if (
+      this.diceOne === this.diceTwo ||
+      this.diceTwo === this.diceThree ||
+      this.diceThree === this.diceOne
+    ) {
+      this.double = true;
+    } else {
+      this.double = false;
+      this.passOnDice = 'Please pass on the dice';
+    }
+    //Checking for Tripple
+    if (
+      this.diceOne === this.diceTwo &&
+      this.diceTwo === this.diceThree &&
+      this.diceThree === this.diceOne
+    ) {
+      this.triple = true;
+      this.double = false;
+    }
+    // Check for Max spin
+    if (
+      this.diceOne === this.diceTwo &&
+      this.diceTwo === this.diceThree &&
+      this.diceThree === this.diceOne &&
+      this.total === 18
+    ) {
+      this.triple = false;
+      this.double = false;
+      this.maxSpinMessage;
+    }
+  }
+}
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  public clock;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.clock = new Observable<string>((observer: Observer<string>) => {
+      setInterval(() => observer.next(new Date().toString()), 1000);
+    });
+  }
+}
